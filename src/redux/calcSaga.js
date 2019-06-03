554b4a8776c67e88885e3c1e4ba31d59762572c7
Rachel Peddie <rachel.peddie.915@gmail.com>
2019-06-02 22:17:59 -0500

    created functions to post new calculation and get last 10 from database

--- src/redux/calcSaga.js
+++ src/redux/calcSaga.js
@@ -1,6 +1,29 @@
 import { takeLatest, put } from 'redux-saga/effects';
 import axios from 'axios';
 
+function* saveCalculation( action ) {
+    console.log( `in saveCalculation saga`, action.payload );
+    try {
+        yield axios.post( '/calculations', action.payload );
+    }
+    catch ( error ){
+        console.log(`sorry, couldn't save the calculation`, error);
+        alert(`There was an error saving your calculation.  Try again later.`)
+    }
+}
+
+function* getLastCalculations( action ) {
+    console.log(`in getLastCalculations saga`);
+    try {
+        const response = yield axios.get( '/calculations');
+        yield put({ type: 'SET_CALCULATIONS', payload: response.data });
+    }
+    catch ( error ) {
+        console.log(`sorry, couldn't get the last 10 calculations`, error);
+        alert(`There was an error retrieving the last 10 calculations.  Try again later.`);
+    }
+}
+
 function* calcSaga() {
     yield takeLatest( 'SAVE_CALCULATION', saveCalculation );
     yield takeLatest( 'GET_LAST_CALCULATIONS', getLastCalculations );
