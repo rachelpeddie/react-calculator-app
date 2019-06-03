afe863902d53d5d5210a89bdcf91fde9582199f1
Rachel Peddie <rachel.peddie.915@gmail.com>
2019-06-03 15:31:59 -0500

    added more code for heroku build

--- server/server.js
+++ server/server.js
@@ -9,6 +9,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 }
+const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 app.use(express.static('server/public'));
 
 // ROUTES
