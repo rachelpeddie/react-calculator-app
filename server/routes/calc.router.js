0fbc8b7a2a2895465dee1faaeead3621b595cff4
Rachel Peddie <rachel.peddie.915@gmail.com>
2019-06-02 21:38:48 -0500

    set up pool.js, set up server.js, set up calc.router.js, created get and post requests

---
+++ server/routes/calc.router.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+
+// DB CONNECTION
+const pool = require('./pool');
+
+module.exports = router;
+
+// get last 10 operations stored in database
+router.get('/', ( req, res ) => {
+    console.log( `in calc.router get` );
+    pool.query( `SELECT * FROM "calculations" DESC LIMIT 10;` )
+    .then( result => {
+        res.send( result.rows );
+    }).catch( error => {
+        console.log( `error getting the latest calculations from the database`, error );
+        res.sendStatus( 500 );
+    })
+})
+
+// post new equation and result to database
+router.post('/', ( req, res ) => {
+    console.log( `in calc.router post`, req.body );
+    let equation = req.body.calculation;
+    let solution = req.body.result;
+    let sqlText = `INSERT INTO "calculations" ("equation", "solution")
+                   VALUES ($1, $2);`;
+    pool.query( sqlText, [equation, solution])
+    .then( response => {
+        res.sendStatus( 201 );
+    }).catch( error => {
+        console.log( `error adding your new calculation to the database`, error );
+        res.sendStatus( 500 );
+    })
+})
